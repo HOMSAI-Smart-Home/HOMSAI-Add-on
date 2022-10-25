5ea1655c11b37ad29accb8f5e1dea58117d176b4
Giacomo Agostini <agostini@hastega.it>
2022-10-25 16:01:57 +0000

    update to v0.8.3

--- homsai/rootfs/var/www/web/main.dart.js_2.part.js
+++ homsai/rootfs/var/www/web/main.dart.js_2.part.js
@@ -1,9 +1,9 @@
 self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
-$__dart_deferred_initializers__.current=function(a,b,c,$){var A={lP:function lP(){}},B
+$__dart_deferred_initializers__.current=function(a,b,c,$){var A={m0:function m0(){}},B
 A=a.updateHolder(c[5],A)
 B=c[0]
-A.lP.prototype={}
+A.m0.prototype={}
 var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
-y(A.lP,B.F)})()
-B.b4m(b.typeUniverse,JSON.parse("{}"))}
-$__dart_deferred_initializers__["jQKLtqe9wGpZyUF03c227gT24Pk="] = $__dart_deferred_initializers__.current
+y(A.m0,B.H)})()
+B.b6m(b.typeUniverse,JSON.parse("{}"))}
+$__dart_deferred_initializers__["WO0fq5IUmXZdUnK2KQ/tm8iZ9Kw="] = $__dart_deferred_initializers__.current
